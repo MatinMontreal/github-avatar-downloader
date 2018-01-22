67c7c126cb128d6378e0ddc1cceeee6047cc01ff
Mat <MatYoung@aol.com>
2018-01-22 23:55:46 +0000

    Updated download.avatars.js with link to secret.js

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,18 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorizaton':'token 700ba2381004cc8e1abb61a5ab4eb680e624d298'
+    }
+  };
+
+  request(options, function(err,res,body){
+  cb(err, body);
+  });
+
 }
 
 
