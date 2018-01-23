f1bac228100c1a1cfdc9ecdddce7e7c0b42ae0c4
Mat <MatYoung@aol.com>
2018-01-23 01:15:36 +0000

    Completed through Step 7

--- download_avatars.js
+++ download_avatars.js
@@ -5,24 +5,34 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-    url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    url:
+      'https://api.github.com/repos/' +
+        repoOwner +
+        '/' +
+        repoName +
+        '/contributors',
     headers: {
       'User-Agent': 'request',
-      'Authorization': secrets.GITHUB_TOKEN
-    }
+      Authorization: secrets.GITHUB_TOKEN,
+    },
   };
 
-  request(options, function(err,res,body){
-  cb(err, body);
-  });
+  request(options, function(err, res, body) {
+    var contributors = JSON.parse(body);
+    var avatarURLs = [];
+    for (i=0; i < contributors.length; i++) {
+    avatarURLs.push(contributors[i].avatar_url)
+    }
 
+    cb(err, avatarURLs);
+
+
+  });
 }
 
-
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+getRepoContributors('jquery', 'jquery', function(err, result) {
+  console.log('Errors:', err);
+  console.log('Result:', result);
 });
 
 
@@ -30,3 +40,4 @@ getRepoContributors("jquery", "jquery", function(err, result) {
 
 
 
+//var obj = JSON.parse('body');
